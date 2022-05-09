93bc85844c539a81b3a31eb8e76f7efb0d5aac63
Peter Becich <peterbecich@gmail.com>
2022-05-08 23:39:00 -0700

    test HPC patch

--- default.nix
+++ default.nix
@@ -1,13 +1,28 @@
-# default.nix
-let 
-  pkgs = import <nixpkgs> { };
-in 
-  pkgs.haskellPackages.developPackage {
-    root = ./.;
-    modifier = drv:
-      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
-        [ cabal-install
-          ghcid
-        ]);
-  }
+# https://input-output-hk.github.io/haskell.nix/tutorials/getting-started/
+let
+  sources = import ./nix/sources.nix {};
+  haskellNix = import sources.haskellNix {};
+  pkgs = import
+    haskellNix.sources.nixpkgs-2111
+    haskellNix.nixpkgsArgs;
 
+in
+  pkgs.haskell-nix.project {
+    projectFileName = "cabal.project";
+    modules = [
+      { doCheck = true;
+        doCoverage = true;
+        doHaddock = true;
+        reinstallableLibGhc = true;
+        doHoogle = false;
+        enableLibraryProfiling = false;
+        packages.mtl-compat.doCoverage = false;
+      }
+    ];
+
+    src = pkgs.haskell-nix.haskellLib.cleanGit {
+      name = "pandoc";
+      src = ./.;
+    };
+    compiler-nix-name = "ghc8107";
+  }
